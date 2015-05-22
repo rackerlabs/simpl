1007f49cb632173047c9d224f788cbb938b6af81
Sam Stavinoha <smlstvnh@gmail.com>
2015-05-22 16:48:42 -0500

    rename class to ThreadLocal and make repr

--- simpl/threadlocal.py
+++ simpl/threadlocal.py
@@ -6,7 +6,7 @@ THREAD_STORE = threading.local()
 DEFAULT_NAMESPACE = 'call_context'
 
 
-class LocalDict(collections.MutableMapping):
+class ThreadLocalDict(collections.MutableMapping):
 
     """A dict whose data is local to the thread."""
 
@@ -14,6 +14,11 @@ class LocalDict(collections.MutableMapping):
         self.varname = varname
         self.args = args
         self.kwargs = kwargs
+
+    def __repr__(self):
+
+        under = repr(self._get_local_dict())
+        return '<%s %s>' % (type(self).__name__, under)
 
     def _get_local_dict(self):
         """Retrieve (or initialize) the thread-local data to use."""
