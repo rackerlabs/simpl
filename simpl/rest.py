191227ceebce6ad95fb26df966864a4e328b3b6d
Ziad Sawalha <gerrit@highbridgellc.com>
2015-07-01 02:09:16 +0300

    feat(rest): add `paginated` decorator
    An opinionated, paginated response formatter.

--- simpl/rest.py
+++ simpl/rest.py
@@ -14,7 +14,11 @@
 
 """REST-ful API Utilites."""
 
+import functools
+
 import bottle
+
+MAX_PAGE_SIZE = 10000000
 
 
 def body(schema=None, types=None, required=False, default=None):
@@ -53,3 +57,174 @@ def body(schema=None, types=None, required=False, default=None):
             return fxn(data, *args)
         return wrapped
     return wrap
+
+
+def paginated(resource_name=None):
+    """Decorator that handles pagination headers, params, and links.
+
+    This accepts, parses, validates, and handles `limit` and `offset` optional
+    query params according to common Rackspace APIs and passes them as kwargs
+    to the decorated function.
+
+        offset: The pagination offset.
+        limit: The pagination limit (or page size). The response is paginated
+            with a default limit of 100 and a maximum limit of 1000.
+
+    Headers returned include:
+        Content-Range: ... per HTTP RFC and 206 response
+        Link: ... one for each page (first, last, next, previous)
+
+    It adds Content-Range headers to the response according to RFC 7233
+    section 4.2 - https://tools.ietf.org/html/rfc7233#section-4.2. The resource
+    name is supplied in the decorator call as resource_name or deduced from
+    uripath if not supplied.
+
+    It adds link headers based on RFC 5988 (by ex-Racker Mark Nottingham) -
+    https://tools.ietf.org/html/rfc5988#section-5.5.
+
+    Future changes to the RFCs or common API concepts, such as adding links to
+    the response body, may be implemented here to consistently handle
+    pagination across all projects using simpl pagination for their APIs.
+
+    Opinionated assumptions:
+    - body has data items under a `data` or `results` key.
+    - the `len()` of that data represents the number of records being returned
+      in this page.
+    - body has a `collection-count` value with the total number of records in
+      the underlying collection.
+    - bottle is being used and this is decorating a route function.
+
+    Responses:
+    - paginated responses are returned as `206 Partial Content` unless all the
+      data fits in one page, inwhich case the response is `200 OK`.
+
+    Example response for **GET** `/widgets[&limit=2&offset=3]`
+
+    > HTTP/1.0 206 Partial Content
+    > Content-Range: widget 3-4/6
+    > Content-Length: 217
+    > Content-Type: application/json
+    > Link: </widgets?limit=2&offset=1>; rel="previous"; title="Previous page"
+    > Link: </widgets?limit=2>; rel="first"; title="First page"
+    > Link: </widgets?offset=4>; rel="last"; title="Last page"
+    ```json
+    {
+        "collection-count": 6,
+        "data": [
+            {
+                "id": 1,
+                "name": "foo"
+            },
+            {
+                "id": 2,
+                "name": "bar"
+            }
+        ]
+    }
+    ```
+    """
+    def _paginated(fxn):
+        """Add pagination (optional) and headers to response."""
+        def _decorator(*args, **kwargs):
+            """Internal function wrapped as a decorator."""
+            try:
+                validate_range_values(bottle.request, 'offset', kwargs)
+                validate_range_values(bottle.request, 'limit', kwargs)
+            except ValueError:
+                bottle.response.status = 416
+                bottle.response.set_header(
+                    'Content-Range', '%s */*' %
+                    resource_name or bottle.request.path.split('/')[-1])
+                return
+
+            data = fxn(*args, **kwargs)
+            write_pagination_headers(
+                data,
+                int(kwargs.get('offset') or 0),
+                int(kwargs.get('limit') or 100),
+                bottle.response,
+                bottle.request.path,
+                resource_name)
+            return data
+        return functools.wraps(fxn)(_decorator)
+    return _paginated
+
+
+def validate_range_values(request, label, kwargs):
+    """Ensure value contained in label is a positive integer."""
+    value = kwargs.get(label, request.query.get(label))
+    if value:
+        kwargs[label] = int(value)
+        if kwargs[label] < 0 or kwargs[label] > MAX_PAGE_SIZE:
+            raise ValueError
+
+
+def write_pagination_headers(data, offset, limit, response, uripath,
+                             resource_name):
+    """Add pagination headers to the bottle response.
+
+    See docs in :func:`paginated`.
+    """
+    items = data.get('results') or data.get('data') or {}
+    count = len(items)
+    try:
+        total = int(data['collection-count'])
+    except (ValueError, TypeError, KeyError):
+        total = None
+    if total is None and offset == 0 and (limit is None or limit > len(items)):
+        total = count
+
+    # Set 'content-range' header
+    response.set_header(
+        'Content-Range',
+        "%s %d-%d/%s" % (resource_name, offset, offset + max(count - 1, 0),
+                         total if total is not None else '*')
+    )
+
+    partial = False
+    if offset:
+        partial = True  # Any offset automatically means we've skipped data
+    elif total is None and count == limit:
+        # Unknown total, but first page is full (so there may be more)
+        partial = True  # maybe more records in next page
+    elif total > count:
+        # Known total and not all records returned
+        partial = True
+
+    if partial:
+        uripath = uripath.strip('/')
+        response.status = 206  # Partial
+
+        # Add Next page link to http header
+        if total is None or (offset + limit) < total - 1:
+            nextfmt = (
+                '</%s?limit=%d&offset=%d>; rel="next"; title="Next page"')
+            response.add_header(
+                "Link", nextfmt % (uripath, limit, offset + limit)
+            )
+
+        # Add Previous page link to http header
+        if offset > 0 and (offset - limit) >= 0:
+            prevfmt = ('</%s?limit=%d&offset=%d>; rel="previous"; '
+                       'title="Previous page"')
+            response.add_header(
+                "Link", prevfmt % (uripath, limit, offset - limit)
+            )
+
+        # Add first page link to http header
+        if offset > 0:
+            firstfmt = '</%s?limit=%d>; rel="first"; title="First page"'
+            response.add_header(
+                "Link", firstfmt % (uripath, limit))
+
+        # Add last page link to http header
+        if (total is not None and  # can't calculate last page if unknown total
+                limit and  # if no limit, then any page is the last page!
+                limit < total):
+            lastfmt = '</%s?offset=%d>; rel="last"; title="Last page"'
+            if limit and total % limit:
+                last_offset = total - (total % limit)
+            else:
+                last_offset = total - limit
+            response.add_header(
+                "Link", lastfmt % (uripath, last_offset))
