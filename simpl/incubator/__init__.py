b973fb130d486ede9c59023ebd4a124636c36bbe
Ziad Sawalha <gerrit@highbridgellc.com>
2015-07-02 18:34:59 +0300

    feat(incubator): create incubator module

---
+++ simpl/incubator/__init__.py
@@ -0,0 +1,15 @@
+"""Other Shared Code of possibly Dubious Quality.
+
+This module contains code that is/was used in more than one of our projects and
+was moved here in order to avoid duplicate code. The intent was:
+- not let code diverge (good or bad - better to fix it in one place)
+- leverage tests and bug fixes from one repo in other repos
+
+Why "dubious quality"?!
+
+This module may contain code, patterns (anti-patterns) that may not withstand
+scrutiny for style, design, etc... The ONLY goal of this repo is to avoid
+duplicating code. Code here may get cleaned up and graduated into simpl proper
+or, if its not good, deleted once we have had the chance to replace or
+decommission it from other repos.
+"""
